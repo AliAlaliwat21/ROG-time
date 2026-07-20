59a8cabf077d439ee0480b93a0114d17908d7444
Ali <Alialaliwat21@gmail.com>
2026-07-21 01:05:05 +0300

    prevent duplicate movie log titles per user

--- controllers/movie-logs.js
+++ controllers/movie-logs.js
@@ -12,12 +12,23 @@ const showNewMovieLogForm = (req,res)=>{
     res.render('movie-log/new.ejs')
 }
 
-const createMovieLog = async(req,res)=>{
-req.body.user = req.session.user._id
-req.body.containsSpoilers = req.body.containsSpoilers === 'on'
-await MovieLog.create(req.body)
+const createMovieLog = async (req, res) => {
+    req.body.user = req.session.user._id
 
-res.redirect('/movie-logs')
+    const existingLog = await MovieLog.findOne({
+        user: req.body.user,
+        title: req.body.title
+    })
+
+    if (existingLog) {
+        return res.send('You have already logged this movie.')
+    }
+
+    req.body.containsSpoilers = req.body.containsSpoilers === 'on'
+
+    await MovieLog.create(req.body)
+
+    res.redirect('/movie-logs')
 }
 
 module.exports = {
